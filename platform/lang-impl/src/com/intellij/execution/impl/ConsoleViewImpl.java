3b9c6477d5b5c4a20a991658726c67293c7696e8
Siva Velusamy <vsiva@google.com>
2014-01-29 19:10:30 -0800

    log console: Customize list of actions shown in popup menu.
    The list of actions shown in the console toolbar is customized
    via an extension point, but the list of actions shown in the popup
    menu aren't. This CL performs the same customization for the
    pop-up menu actions.
    
    This is a fix for https://code.google.com/p/android/issues/detail?id=64180
    
    Change-Id: I0646db950ff391dcc15a34b82009e50f95a52299

--- platform/lang-impl/src/com/intellij/execution/impl/ConsoleViewImpl.java
+++ platform/lang-impl/src/com/intellij/execution/impl/ConsoleViewImpl.java
@@ -922,7 +922,24 @@ public class ConsoleViewImpl extends JPanel implements ConsoleView, ObservableCo
     if (group == null) {
       group = (ActionGroup)actionManager.getAction(CONSOLE_VIEW_POPUP_MENU);
     }
-    final ActionPopupMenu menu = actionManager.createActionPopupMenu(ActionPlaces.EDITOR_POPUP, group);
+
+    /** process actions just like in {@link #createConsoleActions()} below. */
+    ConsoleActionsPostProcessor[] postProcessors = Extensions.getExtensions(ConsoleActionsPostProcessor.EP_NAME);
+    AnAction[] result = group.getChildren(null);
+    for (ConsoleActionsPostProcessor postProcessor : postProcessors) {
+      result = postProcessor.postProcess(this, result);
+    }
+
+    final AnAction[] processedActions = result;
+    ActionGroup processedGroup = new ActionGroup() {
+      @NotNull
+      @Override
+      public AnAction[] getChildren(@Nullable AnActionEvent e) {
+        return processedActions;
+      }
+    };
+
+    final ActionPopupMenu menu = actionManager.createActionPopupMenu(ActionPlaces.EDITOR_POPUP, processedGroup);
     menu.getComponent().show(mouseEvent.getComponent(), mouseEvent.getX(), mouseEvent.getY());
   }
 
