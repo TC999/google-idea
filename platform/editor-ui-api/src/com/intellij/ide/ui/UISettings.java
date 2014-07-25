a29e161ae9658125ec70ff79a688693c2e403628
Benjamin Wulfe <benwu@google.com>
2014-07-25 10:38:09 -0400

    Adds the ability to right align actions on toolbars (specifically used for login).
    Adds a new flag to right align secondary actions to ActionToolbarImpl.  If the UISetting is turned on,
    the main and nav toolbars will set this flag.  Android Studio turns on this setting.
    
    Change-Id: Ib2c5f3757f93a51c1c3bd750fa75b9a44e309502

--- platform/editor-ui-api/src/com/intellij/ide/ui/UISettings.java
+++ platform/editor-ui-api/src/com/intellij/ide/ui/UISettings.java
@@ -116,6 +116,7 @@ public class UISettings implements PersistentStateComponent<UISettings>, Exporta
   public boolean MARK_MODIFIED_TABS_WITH_ASTERISK = false;
   public boolean SHOW_TABS_TOOLTIPS = true;
   public boolean SHOW_DIRECTORY_FOR_NON_UNIQUE_FILENAMES = false;
+  public boolean SHOW_SECONDARYACTIONS_RIGHTALIGNED = false;
 
   private final EventListenerList myListenerList;
 
