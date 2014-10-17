819edea1ea5b302e0887818d837d7e859c930689
Esteban de la Canal <estebandlc@google.com>
2014-10-17 15:53:07 -0700

    Updater: Fixes directory handling
    Directories were completely ignored before, now they are handled
    properly as another file.
    
    This fixes several issues:
      . Deleted directories were ignored leaving empty directories
        behind.
      . Empty directories in the original folder where not recreated in
        the target.
      . When reverting a patch that failed to apply, newly created
        directories were left empty.
      . It is now possible to emit a patch that will delete an empty
        directory.
    
    Change-Id: Ia2e93150ed69146b77de8462388ddb146d4796a7

--- updater/testSrc/com/intellij/updater/PatchFileCreatorTest.java
+++ updater/testSrc/com/intellij/updater/PatchFileCreatorTest.java
@@ -228,7 +228,7 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
   public void testApplyWhenNewFileExists() throws Exception {
     Patch patch = PatchFileCreator.create(myPatchSpec, myFile, TEST_UI);
 
-    FileUtil.writeToFile(new File(myOlderDir, "newfile.txt"),"hello");
+    FileUtil.writeToFile(new File(myOlderDir, "newfile.txt"), "hello");
 
     PatchFileCreator.PreparationResult preparationResult = PatchFileCreator.prepareAndValidate(myFile, myOlderDir, TEST_UI);
     assertTrue(preparationResult.validationResults.isEmpty());
@@ -243,7 +243,6 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
     Patch patch = PatchFileCreator.create(myPatchSpec, myFile, TEST_UI);
 
     FileUtil.writeToFile(new File(myOlderDir, "newfile.txt"), "hello");
-    FileUtil.writeToFile(new File(myOlderDir, "lib/java_pid1234.hprof"), "bye!");
 
     PatchFileCreator.PreparationResult preparationResult = PatchFileCreator.prepareAndValidate(myFile, myOlderDir, TEST_UI);
     assertEquals(1, preparationResult.validationResults.size());
@@ -263,6 +262,40 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
 
     PatchFileCreator.PreparationResult preparationResult = PatchFileCreator.prepareAndValidate(myFile, myOlderDir, TEST_UI);
     assertEquals(0, preparationResult.validationResults.size());
+    assertAppliedAndRevertedCorrectly(patch, preparationResult);
+  }
+
+  @Test
+  public void testApplyWhenNewDirectoryExistsStrict() throws Exception {
+    myPatchSpec.setStrict(true);
+    new File(myOlderDir, "delete").mkdirs();
+    FileUtil.writeToFile(new File(myOlderDir, "delete/deleteme.txt"), "bye!");
+
+    Patch patch = PatchFileCreator.create(myPatchSpec, myFile, TEST_UI);
+
+    new File(myOlderDir, "unexpected_newdir").mkdirs();
+    FileUtil.writeToFile(new File(myOlderDir, "unexpected_newdir/unexpected.txt"), "bye!");
+
+    new File(myOlderDir, "newDir").mkdir();
+
+    PatchFileCreator.PreparationResult preparationResult = PatchFileCreator.prepareAndValidate(myFile, myOlderDir, TEST_UI);
+    assertEquals(3, preparationResult.validationResults.size());
+    assertEquals(new ValidationResult(ValidationResult.Kind.CONFLICT,
+                                      "unexpected_newdir/unexpected.txt",
+                                      ValidationResult.Action.VALIDATE,
+                                      "Unexpected file",
+                                      ValidationResult.Option.DELETE), preparationResult.validationResults.get(0));
+    assertEquals(new ValidationResult(ValidationResult.Kind.CONFLICT,
+                                      "unexpected_newdir/",
+                                      ValidationResult.Action.VALIDATE,
+                                      "Unexpected file",
+                                      ValidationResult.Option.DELETE), preparationResult.validationResults.get(1));
+    assertEquals(new ValidationResult(ValidationResult.Kind.CONFLICT,
+                                      "newDir/",
+                                      ValidationResult.Action.CREATE,
+                                      ValidationResult.ALREADY_EXISTS_MESSAGE,
+                                      ValidationResult.Option.REPLACE), preparationResult.validationResults.get(2));
+    new File(myOlderDir, "newDir").delete();
     assertAppliedAndRevertedCorrectly(patch, preparationResult);
   }
 
@@ -288,7 +321,7 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
     throws IOException, OperationCancelledException {
 
     Map<String, Long> original = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
-
+    Map<String, Long> target = patch.digestFiles(myNewerDir, Collections.<String>emptyList(), TEST_UI);
     File backup = getTempFile("backup");
 
     for (ValidationResult each : preparationResult.validationResults) {
@@ -297,13 +330,19 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
 
     List<PatchAction> appliedActions =
       PatchFileCreator.apply(preparationResult, new HashMap<String, ValidationResult.Option>(), backup, TEST_UI).appliedActions;
-    assertAppliedCorrectly();
+    Map<String, Long> patched = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
 
-    assertFalse(original.equals(patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI)));
+    if (patch.isStrict()) {
+      assertEquals(patched, target);
+    } else {
+      assertAppliedCorrectly();
+    }
+
+    assertNotEquals(original, patched);
 
     PatchFileCreator.revert(preparationResult, appliedActions, backup, TEST_UI);
-
-    assertEquals(original, patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI));
+    Map<String, Long> reverted = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
+    assertEquals(original, reverted);
   }
 
   protected void assertAppliedCorrectly() throws IOException {
@@ -362,7 +401,7 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
 
   private static class MyFailOnApplyPatchAction extends PatchAction {
     public MyFailOnApplyPatchAction(Patch patch) {
-      super(patch, "_dummy_file_", -1);
+      super(patch, "_dummy_file_", Digester.INVALID);
     }
 
     @Override
