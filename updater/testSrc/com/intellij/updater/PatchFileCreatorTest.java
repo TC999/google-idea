01a051171fa2ee2b3623a85fe2fd7624c396f324
Esteban de la Canal <estebandlc@google.com>
2014-10-22 00:29:51 -0700

    Updater: Adds option to normalize source zip files.
    When this option is enabled source zipfiles or jarfiles will be
    normalized before they are used for any purpose. This gives a
    base reference for binary diffs and yields a reasonable "small" patch.

--- updater/testSrc/com/intellij/updater/PatchFileCreatorTest.java
+++ updater/testSrc/com/intellij/updater/PatchFileCreatorTest.java
@@ -59,12 +59,12 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
 
       PatchFileCreator.PreparationResult preparationResult = PatchFileCreator.prepareAndValidate(myFile, myOlderDir, TEST_UI);
 
-      Map<String, Long> original = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
+      Map<String, Long> original = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), false, TEST_UI);
 
       File backup = getTempFile("backup");
       PatchFileCreator.apply(preparationResult, new HashMap<String, ValidationResult.Option>(), backup, TEST_UI);
 
-      assertEquals(original, patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI));
+      assertEquals(original, patch.digestFiles(myOlderDir, Collections.<String>emptyList(), false, TEST_UI));
     }
     finally {
       raf.close();
@@ -303,7 +303,7 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
     assertTrue(action instanceof UpdateAction);
     UpdateAction update = (UpdateAction)action;
     assertTrue(update.isMove());
-    assertEquals("/a/deleted/file/that/is/a/copy/move.me", update.getSource(new File("/")).getAbsolutePath());
+    assertEquals("a/deleted/file/that/is/a/copy/move.me", update.getSourcePath());
 
     PatchFileCreator.PreparationResult preparationResult = PatchFileCreator.prepareAndValidate(myFile, myOlderDir, TEST_UI);
     assertAppliedAndRevertedCorrectly(patch, preparationResult);
@@ -338,7 +338,7 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
     assertTrue(action instanceof UpdateAction);
     UpdateAction update = (UpdateAction)action;
     assertTrue(!update.isMove());
-    assertEquals("/move/from/this/directory/move.me", update.getSource(new File("/")).getAbsolutePath());
+    assertEquals("move/from/this/directory/move.me", update.getSourcePath());
 
     PatchFileCreator.PreparationResult preparationResult = PatchFileCreator.prepareAndValidate(myFile, myOlderDir, TEST_UI);
     assertAppliedAndRevertedCorrectly(patch, preparationResult);
@@ -361,9 +361,9 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
 
   private void assertNothingHasChanged(Patch patch, PatchFileCreator.PreparationResult preparationResult, Map<String, ValidationResult.Option> options)
     throws Exception {
-    Map<String, Long> before = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
+    Map<String, Long> before = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), false, TEST_UI);
     PatchFileCreator.apply(preparationResult, options, TEST_UI);
-    Map<String, Long> after = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
+    Map<String, Long> after = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), false, TEST_UI);
 
     DiffCalculator.Result diff = DiffCalculator.calculate(before, after, new LinkedList<String>(), false);
     assertTrue(diff.filesToCreate.isEmpty());
@@ -374,8 +374,8 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
   private void assertAppliedAndRevertedCorrectly(Patch patch, PatchFileCreator.PreparationResult preparationResult)
     throws IOException, OperationCancelledException {
 
-    Map<String, Long> original = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
-    Map<String, Long> target = patch.digestFiles(myNewerDir, Collections.<String>emptyList(), TEST_UI);
+    Map<String, Long> original = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), false, TEST_UI);
+    Map<String, Long> target = patch.digestFiles(myNewerDir, Collections.<String>emptyList(), false, TEST_UI);
     File backup = getTempFile("backup");
 
     HashMap<String, ValidationResult.Option> options = new HashMap<String, ValidationResult.Option>();
@@ -391,7 +391,7 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
 
     List<PatchAction> appliedActions =
       PatchFileCreator.apply(preparationResult, options, backup, TEST_UI).appliedActions;
-    Map<String, Long> patched = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
+    Map<String, Long> patched = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), false, TEST_UI);
 
     if (patch.isStrict()) {
       assertEquals(patched, target);
@@ -402,7 +402,7 @@ public abstract class PatchFileCreatorTest extends PatchTestCase {
     assertNotEquals(original, patched);
 
     PatchFileCreator.revert(preparationResult, appliedActions, backup, TEST_UI);
-    Map<String, Long> reverted = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), TEST_UI);
+    Map<String, Long> reverted = patch.digestFiles(myOlderDir, Collections.<String>emptyList(), false, TEST_UI);
     assertEquals(original, reverted);
   }
 
