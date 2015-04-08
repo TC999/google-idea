c603cae6058cd8d41d737a6d1134d3aa8fa144f9
Michal Bendowski <bendowski@google.com>
2015-04-08 18:04:32 +0100

    Introduce RunConfigurationProducerManager.
    This allows projects to ignore certain run configurations.
    
    Change-Id: I03370cb29dfff5e3baa7b4571ecf77147672c264

--- platform/lang-api/src/com/intellij/execution/actions/RunConfigurationProducer.java
+++ platform/lang-api/src/com/intellij/execution/actions/RunConfigurationProducer.java
@@ -15,22 +15,20 @@
  */
 package com.intellij.execution.actions;
 
-import com.intellij.execution.Location;
-import com.intellij.execution.PsiLocation;
-import com.intellij.execution.RunManager;
-import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.*;
 import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Supports creating run configurations from context (by right-clicking a code element in the source editor or the project view). Typically,
@@ -41,6 +39,30 @@ import java.util.List;
  */
 public abstract class RunConfigurationProducer<T extends RunConfiguration> {
   public static final ExtensionPointName<RunConfigurationProducer> EP_NAME = ExtensionPointName.create("com.intellij.runConfigurationProducer");
+
+  public static List<RunConfigurationProducer<?>> getProducers(Project project) {
+    Set<Class<? extends RunConfigurationProducer<?>>>
+      ignoredProducers = RunConfigurationProducerManager.getInstance(project).getIgnoredProducers();
+    RunConfigurationProducer[] allProducers = Extensions.getExtensions(EP_NAME);
+
+    List<RunConfigurationProducer<?>> result = ContainerUtil.newArrayListWithCapacity(allProducers.length);
+    for (RunConfigurationProducer producer : allProducers) {
+      boolean ignored = false;
+      for (Class<?> ignoredProducer : ignoredProducers) {
+        if (ignoredProducer.isAssignableFrom(producer.getClass())) {
+          ignored = true;
+          break;
+        }
+      }
+
+      if (!ignored) {
+        result.add(producer);
+      }
+    }
+
+    return result;
+  }
+
   private final ConfigurationFactory myConfigurationFactory;
 
   protected RunConfigurationProducer(final ConfigurationFactory configurationFactory) {
