5156f0a86ead5a1a226eb297e298f127b6c1883d
Joe Baker-Malone <jbakermalone@google.com>
2015-06-24 10:40:01 -0700

    Using aqua indeterminate state icon in ThreeStateCheckBox
    Change-Id: I9c85057382b6ce7f1578a91f7424547b26e5ee36

--- platform/util/src/com/intellij/util/ui/ThreeStateCheckBox.java
+++ platform/util/src/com/intellij/util/ui/ThreeStateCheckBox.java
@@ -15,6 +15,8 @@
  */
 package com.intellij.util.ui;
 
+import com.intellij.openapi.util.SystemInfo;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ItemEvent;
@@ -111,6 +113,11 @@ public class ThreeStateCheckBox extends JCheckBox {
 
   @Override
   protected void paintComponent(Graphics g) {
+    if (SystemInfo.isMacOSSnowLeopard && !UIUtil.isUnderDarcula() && !UIUtil.isUnderIntelliJLaF()) {
+      paintAquaIndeterminateIcon(g);
+      return;
+    }
+
     super.paintComponent(g);
     switch (getState()) {
       case DONT_CARE:
@@ -148,6 +155,23 @@ public class ThreeStateCheckBox extends JCheckBox {
         break;
       default:
         break;
+    }
+  }
+
+  private void paintAquaIndeterminateIcon(Graphics g) {
+    State initial = getState();
+    try {
+      if (getState() == State.DONT_CARE) {
+        setSelected(true);
+        putClientProperty("JButton.selectedState", "indeterminate");
+      }
+      else {
+        putClientProperty("JButton.selectedState", null);
+      }
+      super.paintComponent(g);
+    }
+    finally {
+      setState(initial);
     }
   }
 }
