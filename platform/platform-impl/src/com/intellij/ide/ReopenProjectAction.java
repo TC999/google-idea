bd2483fae057ba4e752b26a49b58648b6b44703e
David Herman <davidherman@google.com>
2015-07-23 18:11:11 -0700

    Fixed "Remove from List" does nothing in Welcome Screen
    See: https://youtrack.jetbrains.com/issue/IDEA-104447
    
    Change-Id: Id3be84a4dbc45a4cbbef49a627695411cf0a16f0

--- platform/platform-impl/src/com/intellij/ide/ReopenProjectAction.java
+++ platform/platform-impl/src/com/intellij/ide/ReopenProjectAction.java
@@ -15,10 +15,7 @@
  */
 package com.intellij.ide;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
@@ -31,6 +28,9 @@ import java.io.File;
 * @author yole
 */
 public class ReopenProjectAction extends AnAction implements DumbAware {
+
+  public final static DataKey<RemovedProjectListener> REMOVED_LISTENER_KEY = DataKey.create("ReopenProject.RemovedListener");
+
   private final String myProjectPath;
   private final String myProjectName;
 
@@ -55,6 +55,10 @@ public class ReopenProjectAction extends AnAction implements DumbAware {
                                        "If it is on a removable or network drive, please make sure that the drive is connected.",
                                        "Reopen Project", new String[]{"OK", "&Remove From List"}, 0, Messages.getErrorIcon()) == 1) {
         RecentProjectsManager.getInstance().removePath(myProjectPath);
+        RemovedProjectListener listener = e.getData(REMOVED_LISTENER_KEY);
+        if (listener != null) {
+          listener.projectRemoved(myProjectPath);
+        }
       }
       return;
     }
@@ -64,8 +68,16 @@ public class ReopenProjectAction extends AnAction implements DumbAware {
   public String getProjectPath() {
     return myProjectPath;
   }
-  
+
   public String getProjectName() {
     return myProjectName;
+  }
+
+  /**
+   * A listener which is fired anytime this action removes a project, which a caller can register for by adding itself as a
+   * {@link DataProvider} with the {@link #REMOVED_LISTENER_KEY}.
+   */
+  public interface RemovedProjectListener {
+    void projectRemoved(String myProjectPath);
   }
 }
