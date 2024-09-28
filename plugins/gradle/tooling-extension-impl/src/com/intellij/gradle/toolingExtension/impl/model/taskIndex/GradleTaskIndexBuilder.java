e166f1e9feb30005cd2765b2976482f324ba3d32
Josh Friend <jfriend@block.xyz>
2024-09-28 01:38:44 +0000

    Use providers api to avoid isolated projects violations
    The gradle property idea.gradle.do.not.build.tasks is never added
    to subprojects and is always passed in via "-P" on commandline.
    
    Fixes b/364913821
    
    Change-Id: I3ae1234f02527df87cc561bb0800a5e47c9acc83

--- plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/taskIndex/GradleTaskIndexBuilder.java
+++ plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/taskIndex/GradleTaskIndexBuilder.java
@@ -36,7 +36,8 @@ public class GradleTaskIndexBuilder extends AbstractModelBuilderService {
     // This is tested by TaskConfigurationNotTriggeredDuringSyncTest.testTasksAreNotConfiguredDuringSync().
     boolean skipTasks;
     try {
-      skipTasks = Boolean.parseBoolean(String.valueOf(project.getProperties().get("idea.gradle.do.not.build.tasks")).trim());
+      Provider<String> buildTasksProvider = project.getProviders().gradleProperty("idea.gradle.do.not.build.tasks");
+      skipTasks = Boolean.parseBoolean(buildTasksProvider.get().trim());
     } catch (Throwable ignored) {
       skipTasks = false;
     }
